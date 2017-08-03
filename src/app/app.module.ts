39404a346aff189ddc01edc905b4c3354f8a4ab3
ranyoo2367 <30319724+ranyoo2367@users.noreply.github.com>
2017-08-03 14:58:02 -0400

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Camera} from 'ionic-native';
+import { AboutPage } from '../pages/about/about';
+import { Flashcard1Page } from '../pages/flashcard1/flashcard1';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage,
+    Flashcard1Page
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    HomePage,
+    TabsPage,
+    Flashcard1Page
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen, Camera,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
