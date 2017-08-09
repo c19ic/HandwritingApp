50d8b5613dbdb89d550595798597e07d5e8ddf81
Martin <30293098+Martinrosen@users.noreply.github.com>
2017-08-09 10:04:23 -0400

    added wikifuncs, retrieveinfo

--- retrieveinfo.py
+++ retrieveinfo.py
@@ -1,56 +1,29 @@
-import wikipedia
-from PyDictionary import PyDictionary
-import codecs
-import re
+from flask import Flask
 from datetime import datetime
+from wikifuncs import *
 
 #---------------------------------------------
 
-myflash={}
-pageerror=[]
-wikipedia.set_rate_limiting(True)
-linecount=0
+application = Flask(__name__)
 
 #---------------------------------------------
-start = datetime.now()
+start=datetime.now()
 
-with codecs.open('insource.txt', 'r', encoding="utf-8") as f:
-	for line in f:
-		linecount+=1
-		line=re.sub('^[^a-zA-z]*|[^a-zA-Z]*$','',line)
-		try:
-			term=str(wikipedia.page(line,auto_suggest=False))
-			term=term[16:-2]
-			definition=str(wikipedia.WikipediaPage(line).content).split('\n')
-			definition=definition[0]
-			if definition=='':
-				pageerror.append(line)
-			else:
-				myflash[term]=definition
-		except wikipedia.exceptions.PageError:
-			pageerror.append(line)
-		except wikipedia.exceptions.DisambiguationError as e:
-			dis=[]
-			for item in e.options:
-				dis.append(item)
-			term='DisambiguationError: %s' % str(line)
-			definition=''
-			for thing in dis:
-				definition+=(str(thing)+(' | '))
-			definition=definition.strip(' | ')
-			myflash[term]=definition
-with codecs.open('outsource.txt', 'w', encoding="utf-8") as g:
-	for term in myflash:
-		g.write('-'*30+'\n'+term+'\n'*2+myflash[term]+'\n')
-	if len(pageerror)>0:
-		if len(myflash)>0:
-			g.write('\n')
-		g.write('~'*30+'\n'*2)
-		for thing in pageerror:
-			g.write(thing+'\n')
+header_text = '''
+    <html>\n<head> <title>Flash Cards</title> </head>\n<body>'''
 
-end = datetime.now()
+inread()
 
-deltat = end - start
+@application.route('/')
+def main():
+	return out()
 
-avgt = deltat / linecount
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+
+end=datetime.now()
+
+deltat=end-start
+
+avgt=deltat/(len(myflash))
