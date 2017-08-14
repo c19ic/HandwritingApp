bd2c23e870d55caae7e62d34d27b16c149761a12
ranyoo2367 <30319724+ranyoo2367@users.noreply.github.com>
2017-08-14 09:51:22 -0400

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import {Camera} from 'ionic-native';
+import {NavController, ToastController, Platform} from 'ionic-angular';
+import { Camera} from 'ionic-native';
+import { HTTP } from '@ionic-native/http';
+
+
+
+declare var cordova: any;
 
 @Component({
   selector: 'page-home',
@@ -9,36 +15,46 @@ import {Camera} from 'ionic-native';
 export class HomePage {
   public base64Image: string;
   private imageSrc: string;
-  constructor(){
+  private storage: Storage;
+  private http: HTTP;
 
-  }
+  constructor(public navCtrl: NavController){
+   }
 
 public openGallery (): void {
   let cameraOptions = {
-    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: Camera.PictureSourceType.PHOTOLIBRARY,  //get the image from gallery
+    destinationType: Camera.DestinationType.FILE_URI,   //return URI for image
     quality: 100,
     targetWidth: 1000,
     targetHeight: 1000,
-    encodingType: Camera.EncodingType.JPEG,
+    encodingType: Camera.EncodingType.JPEG,             //return the image in JPEG format
     correctOrientation: true
   }
 
   Camera.getPicture(cameraOptions)
-    .then(file_uri => this.imageSrc = file_uri,
-    err => console.log(err));
-}
+        .then(file_uri => this.imageSrc = file_uri,
+                             err => console.log(err));
+          }
 
-takePicture(){
+takePicture():void{
     Camera.getPicture({
-        destinationType: Camera.DestinationType.DATA_URL,
+        destinationType: Camera.DestinationType.NATIVE_URI,
+        sourceType: Camera.PictureSourceType.CAMERA,
         targetWidth: 1000,
-        targetHeight: 1000
-    }).then((imageData) => {
-      // imageData is a base64 encoded string
-        this.base64Image = "data:image/jpeg;base64," + imageData;
+        targetHeight: 1000,
+        saveToPhotoAlbum: true,
+        encodingType: Camera.EncodingType.JPEG
+    }).then(
+        (imageData) => {
+          this.base64Image = "data:image/jpeg;base64," + imageData;
     }, (err) => {
-        console.log(err);
-    });
-  }
-}
+        console.log(err)
+        });
+
+    this.http.post("http://18.220.97.255", this.base64Image, {})
+    }
+    }
+
+
+
